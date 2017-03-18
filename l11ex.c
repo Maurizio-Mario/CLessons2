d06ad4247b1e46e3adad06a6e6b19ae7ed1a471d
Maurizio-Mario <maurizio.murino@hotmail.com>
2017-03-18 20:47:07 +0100

    Lesson 11. Completed.

--- l11ex.c
+++ l11ex.c
@@ -51,6 +51,42 @@ int revSequence(){
 	return 0;
 }
 
+int push2 (float vect[], float **ppcima, float item){
+	if(*ppcima < vect + DIM){
+		**ppcima = item;
+		(*ppcima)++;
+		return 1;
+	}else
+		return 0;
+}
+
+int pop2 (float vect[], float **pptop, float *pitem){
+	if(*pptop > vect) {
+		(*pptop)--;
+		*pitem = **pptop;
+		return 1;
+	}
+	else
+		return 0;
+}
+
+int isEmpty2(float vect[], float *ptop) {
+	return(ptop == vect);
+	}
+
+void revSequence2(){
+	float myStack[DIM];
+	float *top = myStack;
+	float lettura;
+	do {
+		scanf("%f" , &lettura);
+	}while(push2(myStack, &top, lettura) && lettura != 0.0);
+	while(!isEmpty2(myStack, top)){
+		pop2(myStack, &top, &lettura);
+		printf(" %.1f" , lettura);
+	}
+	printf("\n\n");
+}
 
 int main(int argc, char argv[]){
 	printf("\nWelcome to the lesson number 11 of the second semester!\n"
@@ -58,10 +94,13 @@ int main(int argc, char argv[]){
 	int es;
 	do{
 		printf("\t1\tEx. 1:\tInvert input data in array;\n"
+			"\t2\tEx. 2:\tInvert input data in array with pointer to pointers;\n"
 			"\t*\tOther:\tExit\n\n");
 		scanf("%d", &es);
 		if(es == 1)
 			revSequence();
-	}while(es > 0 && es < 2);
+		else if(es == 2)
+			revSequence2();
+	}while(es > 0 && es < 3);
 	return 0;
 } 
